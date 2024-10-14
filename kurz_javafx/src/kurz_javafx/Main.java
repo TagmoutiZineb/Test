4e6e13968cd381bbc540fce13d046169f840c74f
Zineb Tagmouti <146717832+TagmoutiZineb@users.noreply.github.com>
2024-10-14 14:38:49 +0200

    4 push

---
+++ kurz_javafx/src/kurz_javafx/Main.java
@@ -0,0 +1,16 @@
+package kurz_javafx;
+
+	import javafx.application.Application;
+	import javafx.stage. Stage;
+
+	public class Main extends Application {
+
+	@Override
+	public void start (Stage primaryStage) {
+	System. out.println ("Anwendung mit JavaFX") ;
+	}
+	public static void main (String[] args) {
+	launch (args) ;
+
+	}
+}
